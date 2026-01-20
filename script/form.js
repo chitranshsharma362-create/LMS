0347fc25918fa7cd794252e81d3f6d2558871719
chitranshsharma362-create <chitranshsharma362@gmail.com>
2026-01-21 04:22:18 +0530

    Create form.js

---
+++ script/form.js
@@ -0,0 +1,71 @@
+let form = document.getElementById("form");
+
+let nameInput = document.getElementById("name");
+let nameError = document.getElementById("nameError");
+let mailInput = document.getElementById("email");
+let mailError = document.getElementById("mailError");
+let passwordInput = document.getElementById("password");
+let passError = document.getElementById("passError");
+let confirmInput = document.getElementById("confirmpass");
+let confirmError = document.getElementById("confirmError");
+
+let showpass = document.getElementById("showpass");
+
+form.addEventListener("submit" , function(e){
+    e.preventDefault();
+
+    let valid = true;
+
+    if(nameInput.value.length < 3){
+        nameError.innerText = "Minimum 3 characters";
+        valid = false;
+    } else{
+        nameError.innerText = "";
+    }
+
+    let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if(! mailInput.value.match(emailPattern)){
+        mailError.innerText = "Invalid email";
+        valid = false;
+    } else {
+        mailError.innerText = "";
+    }
+
+    let password = passwordInput.value;
+
+    if(password.length < 8 || !(/\d/.test(password)) || !(/[!@#$%^&*]/).test(password)){
+        passError.innerText = "Minimum 8 characters, 1 number & 1 special character required";
+        valid = false;
+    }else{
+        passError.innerText = "";
+    }
+
+    if(password !== confirmInput.value){
+        confirmError.innerText = "Password mismatch";
+        valid = false;
+    } else {
+        confirmError.innerText = "";
+    }
+
+    if(valid){
+
+        let user = {
+            name: nameInput.value,
+            email: mailInput.value,
+            password: passwordInput.value
+        };
+
+        let userString = JSON.stringify(user);
+        localStorage.setItem("userData" , userString);
+
+        alert("Signup Succesfull");
+        form.reset();
+    }
+});
+
+showpass.addEventListener("change" , function(){
+    let type = showpass.checked ?
+    "text" : "password";
+    passwordInput.type = type;
+    confirmInput.type = type;
+});
