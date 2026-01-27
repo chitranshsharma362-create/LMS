c6ccb14c89665a3e9cd47cd5e686c8a995f9ac7f
chitranshsharma362-create <chitranshsharma362@gmail.com>
2026-01-27 11:35:23 +0530

    Update form.js

--- script/form.js
+++ script/form.js
@@ -43,7 +43,7 @@ form.addEventListener("submit", function (e) {
 });
 }
 
-is(showpass){
+if(showpass){
 showpass.addEventListener("change", function () {
     let type = showpass.checked ? "text" : "password";
     passwordInput.type = type;
