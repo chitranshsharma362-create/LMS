81d07e68361a72ffc431319af677170d2ba83b4e
chitranshsharma362-create <chitranshsharma362@gmail.com>
2026-01-27 12:15:11 +0530

    Update form.js

--- script/form.js
+++ script/form.js
@@ -11,43 +11,43 @@ let confirmError = document.getElementById("confirmError");
 let showpass = document.getElementById("showpass");
 
 if(form){
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    let valid = true;
-
-    if (nameInput.value.length < 3) {
-        nameError.innerText = "Minimum 3 characters";
-        valid = false;
-    } else nameError.innerText = "";
-
-    let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-    if (!mailInput.value.match(emailPattern)) {
-        mailError.innerText = "Invalid email";
-        valid = false;
-    } else mailError.innerText = "";
-
-    let password = passwordInput.value;
-    if (password.length < 8 || !(/\d/.test(password)) || !(/[!@#$%^&*]/.test(password))) {
-        passError.innerText = "Minimum 8 characters, 1 number & 1 special character required";
-        valid = false;
-    } else passError.innerText = "";
-
-    if (password !== confirmInput.value) {
-        confirmError.innerText = "Password mismatch";
-        valid = false;
-    } else confirmError.innerText = "";
-
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+    
+        let valid = true;
+    
+        if (nameInput.value.length < 3) {
+            nameError.innerText = "Minimum 3 characters";
+            valid = false;
+        } else nameError.innerText = "";
+    
+        let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+        if (!mailInput.value.match(emailPattern)) {
+            mailError.innerText = "Invalid email";
+            valid = false;
+        } else mailError.innerText = "";
+    
+        let password = passwordInput.value;
+        if (password.length < 8 || !(/\d/.test(password)) || !(/[!@#$%^&*]/.test(password))) {
+            passError.innerText = "Minimum 8 characters, 1 number & 1 special character required";
+            valid = false;
+        } else passError.innerText = "";
+    
+        if (password !== confirmInput.value) {
+            confirmError.innerText = "Password mismatch";
+            valid = false;
+        } else confirmError.innerText = "";
+    
         alert("Signup Successful");
         form.reset();
-});
+    });
 }
 
 if(showpass){
-showpass.addEventListener("change", function () {
-    let type = showpass.checked ? "text" : "password";
-    passwordInput.type = type;
-    confirmInput.type = type;
-});
+    showpass.addEventListener("change", function () {
+        let type = showpass.checked ? "text" : "password";
+        passwordInput.type = type;
+        confirmInput.type = type;
+    });
 }
 
