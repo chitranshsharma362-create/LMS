f1a4146d28cacd8618801c08d6ac187c719a8fe7
chitranshsharma362-create <chitranshsharma362@gmail.com>
2026-01-27 11:48:22 +0530

    Update users.js

--- script/users.js
+++ script/users.js
@@ -1,50 +1,48 @@
-document.getElementById("form").addEventListener("submit" , async (e) => {
+document.getElementById("form").addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const name = document.getElementById("name").value.trim();
   const email = document.getElementById("email").value.trim().toLowerCase();
   const password = document.getElementById("password").value.trim();
 
-  if (!name || !email || !password){
-    alert("all fields are required");
+  if (!name || !email || !password) {
+    alert("All fields are required");
     return;
   }
 
-  const {data: existingUser, error: checkError } =
-    await supabaseClient
-  .from("users")
-  .select("id")
-  .eq("email", email)
-  .maybeSingle();
+  const { data: existingUser, error: checkError } = await supabaseClient
+    .from("users")
+    .select("id")
+    .eq("email", email)
+    .maybeSingle();
 
-  if (checkError){
+  if (checkError) {
     console.error(checkError);
     alert("Error checking user");
     return;
   }
 
   if (existingUser) {
-    alert("Email already registerd");
+    alert("Email already registered");
     return;
   }
 
-  const { error: insertError} =
-    await supabaseClient
-  .from("users")
-  .insert([
-  {
-    name: name,
-    email: email,
-    password: password
-}
-]);
+  const { error: insertError } = await supabaseClient
+    .from("users")
+    .insert([
+      {
+        name,
+        email,
+        password
+      }
+    ]);
 
-if (insertError){
-  console.error(insertError);
-  alert("registration failed");
-  return;
-}
-}
+  if (insertError) {
+    console.error(insertError);
+    alert("Registration failed");
+    return;
+  }
 
-alert("Registration successfully");
-e.target.reset();
+  alert("Registration successfull");
+  e.target.reset();
+});
