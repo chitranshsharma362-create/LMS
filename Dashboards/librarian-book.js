797b6930e219b212283af723d90b4d52d6a3eb7a
chitranshsharma362-create <chitranshsharma362@gmail.com>
2026-01-30 01:45:32 +0530

    Create librarian-book.js

---
+++ Dashboards/librarian-book.js
@@ -0,0 +1,22 @@
+async function addBook() {
+  const name = document.getElementById("").value;
+  const author = document.getElementById("").value;
+  const quantity = document.getElementById("").value;
+
+  const {error} = await supabase
+  .from("books")
+  .insert([
+    {
+      name: name,
+      author: author,
+      quantity: quantity
+    }
+    ]);
+
+if (error) {
+  alert("Book not Added");
+  return;
+}
+
+alert("Successfully Added");
+}
