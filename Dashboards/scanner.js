5e11b9c0169d956bc85a84fb2d3745a46987c47f
chitranshsharma362-create <chitranshsharma362@gmail.com>
2026-07-02 16:30:10 +0530

    Update scanner.js

--- Dashboards/scanner.js
+++ Dashboards/scanner.js
@@ -1,38 +1,23 @@
 let html5QrCode = null;
-
 window.addEventListener("DOMContentLoaded", () => {
-
     const scanBtn = document.getElementById("scanBtn");
-
     if (!scanBtn) {
         console.log("Scan button not found");
         return;
     }
-
     scanBtn.addEventListener("click", () => {
-
         openModal("bookModal");
-
         setTimeout(() => {
             startScanner();
         }, 500);
-
     });
-
 });
-
 async function startScanner() {
-
     const reader = document.getElementById("reader");
-
     reader.style.display = "block";
-
     if (html5QrCode) return;
-
     html5QrCode = new Html5Qrcode("reader");
-
     try {
-
         await html5QrCode.start(
             { facingMode: "environment" },
             {
@@ -41,29 +26,42 @@ async function startScanner() {
             },
             onScanSuccess
         );
-
     } catch (err) {
-
         console.error(err);
         alert("Camera could not start.");
-
     }
-
 }
-
 async function onScanSuccess(decodedText) {
-
     await html5QrCode.stop();
     await html5QrCode.clear();
-
     html5QrCode = null;
-
     document.getElementById("reader").style.display = "none";
-
     const isbn = decodedText.replace(/\D/g, "").substring(0, 13);
-
-    document.getElementById("bookIsbn").value = isbn;
-
-    fetchBook(isbn);
-
+    await fetchBookDetails(isbn);
+}
+// ================= FETCH BOOK DETAILS =================
+async function fetchBookDetails(isbn) {
+    try {
+        const response = await fetch(`https://openlibrary.org/isbn/${isbn}.json`);
+        if (!response.ok) {
+            alert("Book Not Found");
+            return;
+        }
+        const data = await response.json();
+        document.getElementById("bookIsbn").value = isbn;
+        document.getElementById("bookName").value = data.title || "";
+        if (data.authors && data.authors.length > 0) {
+            const authorResponse = await fetch(
+                `https://openlibrary.org${data.authors[0].key}.json`
+            );
+            const author = await authorResponse.json();
+            document.getElementById("bookAuthor").value =
+                author.name || "";
+        } else {
+            document.getElementById("bookAuthor").value = "";
+        }
+    } catch (err) {
+        console.error(err);
+        alert("Book details fetch failed.");
+    }
 }
