d1a83e1284ef5f5a96dfcb9fd87616297014e068
chitranshsharma362-create <chitranshsharma362@gmail.com>
2026-07-02 17:02:06 +0530

    Update form.js

--- script/form.js
+++ script/form.js
@@ -44,54 +44,48 @@ function validateForm() {
 
 
 async function registerUser(event) {
-
     event.preventDefault();
-
     if (!validateForm()) return;
-
     const library_name = document.getElementById("library_name").value.trim();
     const name = document.getElementById("name").value.trim();
     const email = document.getElementById("email").value.trim().toLowerCase();
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirmpass").value;
-
     if (password !== confirmPassword) {
         alert("Password Mismatch");
         return;
     }
-
     try {
-        const { data: existingUser, error: checkError } =
-            await supabaseClient
-                .from("users")
-                .select("user_id")
-                .eq("email", email)
-                .maybeSingle();
-
-        if (checkError) throw checkError;
-        if (existingUser) {
-            alert("Email Already Exists");
-            return;
+        const response = await fetch("http://127.0.0.1:5000/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                library_name,
+                name,
+                email,
+                password
+            })
+        });
+        const result = await response.json();
+        if (response.ok) {
+            alert(result.message);
+            localStorage.setItem("user_id", result.user_id);
+            localStorage.setItem("library_id", result.library_id);
+            localStorage.setItem("library_code", result.library_code);
+            localStorage.setItem("name", result.name);
+            localStorage.setItem("role", result.role);
+            document.getElementById("form").reset();
+            window.location.href = "index.html";
+        } else {
+            alert(result.message);
         }
-
-        let libraryCode;
-
-        while (true) {
-
-            libraryCode =
-                "LIB" +
-                Math.floor(100000 + Math.random() * 900000);
-
-            const { data } =
-                await supabaseClient
-                    .from("libraries")
-                    .select("library_id")
-                    .eq("library_code", libraryCode)
-                    .maybeSingle();
-
-            if (!data) break;
-
-        }
+    } catch (err) {
+        console.error(err);
+        alert("Server Error");
+    }
+}
 
         // Insert Library
 
