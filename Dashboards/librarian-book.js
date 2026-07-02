bed875f05dd148372303745443cafafb764d2550
chitranshsharma362-create <chitranshsharma362@gmail.com>
2026-07-02 16:26:14 +0530

    Update librarian-book.js

--- Dashboards/librarian-book.js
+++ Dashboards/librarian-book.js
@@ -1,1 +1,34 @@
+async function addBookToDB() {
 
+    const isbn = document.getElementById("bookIsbn").value.trim();
+    const name = document.getElementById("bookName").value.trim();
+    const author = document.getElementById("bookAuthor").value.trim();
+    const quantity = Number(document.getElementById("bookQty").value);
+
+    if (!isbn || !name || !author || !quantity) {
+        alert("Fill all fields");
+        return;
+    }
+    const { error } = await supabase
+        .from("books")
+        .insert([
+            {
+                isbn: isbn,
+                book_name: name,
+                author: author,
+                total_quantity: quantity,
+                available_quantity: quantity
+            }
+        ]);
+    if (error) {
+        console.log(error);
+        alert(error.message);
+    } else {
+        alert("Book Added Successfully");
+        document.getElementById("bookIsbn").value = "";
+        document.getElementById("bookName").value = "";
+        document.getElementById("bookAuthor").value = "";
+        document.getElementById("bookQty").value = "";
+        closeModal("bookModal");
+    }
+}
